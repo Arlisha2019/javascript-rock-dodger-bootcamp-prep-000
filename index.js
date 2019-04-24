5feebbb70eadf2addc35d68182bee4d2cc5fc0cb
Arlisha2019 <arlisha_hayles@yahoo.com>
2019-04-24 03:05:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,11 +24,12 @@ function checkCollision(rock) {
 
   // rocks are 20px high
   // DODGER is 20px high
-  // GAME_HEIGHT - 20 - 20 = 360px;
+  // GAME_HEIGHT -20 -20 = 360px;
   if (top > 360) {
     const dodgerLeftEdge = positionToInteger(DODGER.style.left)
 
     // FIXME: The DODGER is 40 pixels wide -- how do we get the right edge?
+    
     const dodgerRightEdge = 0;
 
     const rockLeftEdge = positionToInteger(rock.style.left)
